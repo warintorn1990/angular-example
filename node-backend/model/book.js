671a3e481e59f7d2bf2ee3691e4457d09c79e86c
arm <warintorn.wib.true@gmail.com>
2023-05-01 10:42:08 +0700

    initial commit

---
+++ node-backend/model/book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+let Book = new Schema({
+    name: {
+        type: String
+    },
+    price: {
+        type: String
+    },
+    description: {
+        type: String
+    }
+},{
+    collation: 'books'
+})
+
+module.exports = mongoose.model('Book', Book);
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { BookListComponent } from './components/book-list/book-list.component'
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddBookComponent,
+    BookDetailComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
